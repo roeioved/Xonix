501f4233a37c9f332db60c82770a37830c2f899e
yairlev <yairlevinson@Yairs-MacBook-Pro.local>
2012-03-14 17:34:10 +0200

    changes to game

--- scripts/game.js
+++ scripts/game.js
@@ -5,6 +5,7 @@ function Game(rows, cols, blockSize, frame, ctx) {
     this._cols = cols;
     this._ctx = ctx;
     this._grid;
+    this._player;
     this._monsters = [];
     this._balls = [];
     this._intervalId;
@@ -14,11 +15,20 @@ Game.NUM_OF_LIVES = 10;
 Game.CONQUERED_PERCENT_MINIMUM_LIMIT = 75;
 Game.NUM_OF_MONSTERS = 1;
 Game.NUM_OF_BALLS = 1;
+Game.CONQUERED_CELL_COLOR = '#00A8A8';
+Game.FREE_CELL_COLOR = '#000000';
+Game.BALL_BORDER_COLOR = '#ffffff';
+Game.BALL_FILL_COLOR = '#00A8A8';
+Game.MONSTER_BORDER_COLOR = '#000000';
+Game.MONSTER_FILL_COLOR = '#00A8A8';
 
 Game.prototype = {
 
     init:function () {
         this._grid = new Grid(this._cols, this._rows, 0);
+
+        this._balls = [];
+        this._monsters = [];
 
         for (var i = 0, j = this._cols - 1, k = this._rows - 1; i < this._frame; i++, j--, k--) {
             this._grid.set_row(i, 1);
@@ -29,54 +39,55 @@ Game.prototype = {
 
         //create balls
         for (var i = 0; i < Game.NUM_OF_BALLS; i++) {
-            var col = this._random(this._frame , this._cols - this._frame);
+            var col = this._random(this._frame, this._cols - this._frame);
             var row = this._random(this._frame, this._rows - this._frame);
-            var velocityX = this._speed * (random(0, 1) == 0 ? -1 : 1);
-            var velocityY = this._speed * (random(0, 1) == 0 ? -1 : 1);
+            var velocityX = (this._random(0, 1) == 0 ? -1 : 1);
+            var velocityY = (this._random(0, 1) == 0 ? -1 : 1);
 
-            var ball = new Ball(x, y, this._ballSize / 2, '#00A8A8', 'White', new Vector(velocityX, velocityY), this._innerBoundary, this._arrConquered);
+            var ball = new Ball(row, col, new Vector(velocityX, velocityY), this._grid);
 
-            this._arrBalls.push(ball);
+            this._balls.push(ball);
         }
 
         //create monsters
-        for (var i = 0; i < this._numOfMonsters; i++) {
-            var left = random(0, this._rows - this._monsterSize);
-            var top = random(this._cols - this._frameBorder, this._cols - this._monsterSize);
-            var velocityX = this._speed * (random(0, 1) == 0 ? -1 : 1);
-            var velocityY = this._speed * (random(0, 1) == 0 ? -1 : 1);
-            var monster = new Monster(left, top, this._monsterSize, '#00A8A8', 'Black', new Vector(velocityX, velocityY), this._outerBoundary, this._arrFree);
-            this._arrMonsters.push(monster);
+        for (var i = 0; i < Game.NUM_OF_MONSTERS; i++) {
+
+            var cols_1 = this._random(0, this._frame - 1);
+            var cols_2 = this._random(this._cols - this._frame, this._cols - 1);
+            var col = this._random(0, 1) == 0 ? cols_1 : cols_2;
+
+            var rows_1 = this._random(0, this._frame - 1);
+            var rows_2 = this._random(this._rows - this._frame, this._rows - 1);
+            var row = this._random(0, 1) == 0 ? rows_1 : rows_2;
+
+            var velocityX = random(0, 1) == 0 ? -1 : 1;
+            var velocityY = random(0, 1) == 0 ? -1 : 1;
+
+            var monster = new Monster(row, col, new Vector(velocityX, velocityY), this._grid);
+            this._monsters.push(monster);
         }
+
+        var self = this;
 
         $(document).keydown(function (event) {
             switch (event.keyCode) {
                 case KEY_CODES.LEFT:
-                    if (self._state == GAME_STATES.RUNNING) {
-                        self._player.moveLeft();
-                    }
+                    self._player.moveLeft();
                     break;
                 case KEY_CODES.UP:
-                    if (self._state == GAME_STATES.RUNNING) {
-                        self._player.moveUp();
-                    }
+                    self._player.moveUp();
                     break;
                 case KEY_CODES.RIGHT:
-                    if (self._state == GAME_STATES.RUNNING) {
-                        self._player.moveRight();
-                    }
+                    self._player.moveRight();
                     break;
                 case KEY_CODES.DOWN:
-                    if (self._state == GAME_STATES.RUNNING) {
-                        self._player.moveDown();
-                    }
+                    self._player.moveDown();
                     break;
             }
         });
     },
 
     start:function () {
-
         var self = this;
 
         this._intervalId = setInterval(function () {
@@ -93,11 +104,32 @@ Game.prototype = {
     },
 
     step:function () {
+        for (var i=0; i<this._monsters.length; i++)
+        {
+            this._monsters[i].step();
+        }
 
+        for (var i=0; i<this._balls.length; i++)
+        {
+            this._balls[i].step();
+        }
     },
 
     draw:function () {
 
+        this._grid.draw();
+
+        for (var i=0; i<this._monsters.length; i++)
+        {
+            this._monsters[i].draw(this._ctx, Game.MONSTER_FILL_COLOR, Game.MONSTER_BORDER_COLOR);
+        }
+
+        for (var i=0; i<this._balls.length; i++)
+        {
+            this._balls[i].draw(this._ctx, Game.BALL_FILL_COLOR, Game.BALL_BORDER_COLOR);
+        }
+
+        this._grid.draw(this._ctx, Game.FREE_CELL_COLOR , Game.CONQUERED_CELL_COLOR);
     },
 
     _random:function (min, max) {
