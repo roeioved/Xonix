df25a99df12dfe6dcb448d3c31d870e559e83374
yairlev <yairlevinson@Yairs-MacBook-Pro.local>
2012-03-14 16:34:52 +0200

    added new files of new grid-based xonix

--- scripts/game.js
+++ scripts/game.js
@@ -1,104 +1,36 @@
-function Game(width, height, frameBorder, ctx) {
-    this._width = width;
-    this._height = height;
-    this._frameBorder = frameBorder;
+function Game(rows, cols, blockSize, frame, ctx) {
+    this._blockSize = blockSize;
+    this._frame = frame;
+    this._rows = rows;
+    this._cols = cols;
     this._ctx = ctx;
-    this._numOfLives = Game.NUM_OF_LIVES;
-    this._playerSize = Game.PLAYER_SIZE;
-    this._speed = Game.SPEED;
-    this._player = null;
-    this._numOfMonsters = Game.NUM_OF_MONSTERS;
-    this._monsterSize = 9;
-    this._numOfBalls = 1;
-    this._ballSize = 8;
-    this._arrFree = [];
-    this._arrConquered = [];
-    this._arrBalls = [];
-    this._arrMonsters = [];
-    this._outerBoundary = null;
-    this._innerBoundary = null;
-    this._totalArea = 0;
-    this._conquredArea = 0;
-    this._arrFree = [];
-
-    this._score = 0;
-
-    this._mute = true;
-    this._audio = [];
-    this._audio['fail'] = new Audio('sounds/fail.mp3');
-    this._outerBoundary = null;
-    this._innerBoundary = null;
-
-    this.init();
+    this._grid;
+    this._monsters = [];
+    this._balls = [];
+    this._intervalId;
 }
 
 Game.NUM_OF_LIVES = 10;
-Game.PLAYER_SIZE = 10;
-Game.SPEED = 5;
 Game.CONQUERED_PERCENT_MINIMUM_LIMIT = 75;
-Game.NUM_OF_MONSTERS = 0;
+Game.NUM_OF_MONSTERS = 1;
+Game.NUM_OF_BALLS = 1;
 
 Game.prototype = {
 
     init:function () {
+        this._grid = new Grid(this._cols, this._rows, 0);
 
-        this._arrFree = [];
-        this._arrConquered = [];
-        this._arrBalls = [];
-        this._arrMonsters = [];
-
-        //set border as conquered area
-        this._arrConquered.push(new Polygon([
-            new Point(0, 0),
-            new Point(this._width, 0),
-            new Point(this._width, this._frameBorder),
-            new Point(0, this._frameBorder)
-        ]));
-        this._arrConquered.push(new Polygon([
-            new Point(0, this._height - this._frameBorder),
-            new Point(this._width, this._height - this._frameBorder),
-            new Point(this._width, this._height),
-            new Point(0, this._height)
-        ]));
-        this._arrConquered.push(new Polygon([
-            new Point(0, this._frameBorder),
-            new Point(this._frameBorder, this._frameBorder),
-            new Point(this._frameBorder, this._height - this._frameBorder),
-            new Point(0, this._height - this._frameBorder)
-        ]));
-        this._arrConquered.push(new Polygon([
-            new Point(this._width - this._frameBorder, this._frameBorder),
-            new Point(this._width, this._frameBorder),
-            new Point(this._width, this._height - this._frameBorder),
-            new Point(this._width - this._frameBorder, this._height - this._frameBorder)
-        ]));
-        
-        this._totalArea = (this._width - this._frameBorder) * (this._height - this._frameBorder);
-        this._conquredArea = 0;
-        
-        this._outerBoundary = new Rectangle(0, 0, this._width, this._height);
-        this._innerBoundary = new Rectangle(this._frameBorder, this._frameBorder, this._width - this._frameBorder, this._height - this._frameBorder);
-        
-        //create player
-        this._player = new Player(this._width / 2 - this._playerSize / 2, 0, this._playerSize, this._speed, 'White', '#901290', this._outerBoundary);
-        this._player.addEventListener('conquer', this.onConquer, this);
-        this._player.addEventListener('fail', this.onFail, this);
-        
-        var self = this;
-        
-        //create start free area
-        var free = new Polygon([
-            new Point(this._frameBorder, this._frameBorder),
-            new Point(this._width - this._frameBorder, this._frameBorder),
-            new Point(this._width - this._frameBorder, this._height - this._frameBorder),
-            new Point(this._frameBorder, this._height - this._frameBorder)
-        ]);
-        this._arrFree.push(free);
+        for (var i = 0, j = this._cols - 1, k = this._rows - 1; i < this._frame; i++, j--, k--) {
+            this._grid.set_row(i, 1);
+            this._grid.set_row(j, 1);
+            this._grid.set_col(i, 1);
+            this._grid.set_col(k, 1);
+        }
 
         //create balls
-        for (var i = 0; i < this._numOfBalls; i++) {
-            var x = random(this._frameBorder * 2, this._width - this._frameBorder * 2);
-            var y = random(this._frameBorder * 2, this._height - this._frameBorder * 2);
+        for (var i = 0; i < Game.NUM_OF_BALLS; i++) {
+            var col = this._random(this._frame , this._cols - this._frame);
+            var row = this._random(this._frame, this._rows - this._frame);
             var velocityX = this._speed * (random(0, 1) == 0 ? -1 : 1);
             var velocityY = this._speed * (random(0, 1) == 0 ? -1 : 1);
 
@@ -109,8 +41,8 @@ Game.prototype = {
 
         //create monsters
         for (var i = 0; i < this._numOfMonsters; i++) {
-            var left = random(0, this._width - this._monsterSize);
-            var top = random(this._height - this._frameBorder, this._height - this._monsterSize);
+            var left = random(0, this._rows - this._monsterSize);
+            var top = random(this._cols - this._frameBorder, this._cols - this._monsterSize);
             var velocityX = this._speed * (random(0, 1) == 0 ? -1 : 1);
             var velocityY = this._speed * (random(0, 1) == 0 ? -1 : 1);
             var monster = new Monster(left, top, this._monsterSize, '#00A8A8', 'Black', new Vector(velocityX, velocityY), this._outerBoundary, this._arrFree);
@@ -142,186 +74,35 @@ Game.prototype = {
             }
         });
     },
-    
-    relocatePlayer: function () {
-        this._player.killTrack();
-        var left = this._width / 2 - this._playerSize / 2;
-        var top = 0;
-        this._player._tl = new Point(left, top);
-        this._player._br = new Point(left + this._player._size, top + this._player._size);
-    },
-    
-    start: function () {
-        var self = this;
 
-        this._state = GAME_STATES.RUNNING;
+    start:function () {
+
+        var self = this;
 
         this._intervalId = setInterval(function () {
             try {
-                self._ctx.clearRect(0, 0, self._width, self._height);
+                self._ctx.clearRect(0, 0, self._rows * this._blockSize, self._cols * this._blockSize);
                 self.step();
                 self.draw();
             } catch (err) {
-                clearInterval(this._intervalId);
+                clearInterval(self._intervalId);
                 console.log(err.message);
-                debugger;
             }
         }, 1000 / 30);
+
     },
 
-    stop: function () {
-        clearInterval(this._intervalId);
-        this._state = GAME_STATES.STOPPED;
+    step:function () {
+
     },
 
-    end: function () {
-        this._state = GAME_STATES.ENDED;
-        this.stop();
+    draw:function () {
+
     },
 
-    step: function () {
-        //update ball's obstacles
-        for (var ball in this._arrBalls) {
-            this._arrBalls[ball].set_obstacles(this._arrConquered);
-            this._arrBalls[ball].step();
-        }
-
-        //update monster's obstacles
-        for (var monster in this._arrMonsters) {
-            this._arrMonsters[monster].set_obstacles(this._arrFree);
-            this._arrMonsters[monster].step();
-        }
-
-        //update player's enemies
-        this._player.set_enemies(this.get_enemies());
-        this._player.set_conqueredAreas(this._arrConquered);
-        this._player.set_freeAreas(this._arrFree);
-        this._player.step();
-    },
-
-    draw: function () {
-        this._drawArray(this._ctx, this._arrFree, 'Black');
-        this._drawArray(this._ctx, this._arrConquered, '#00A8A8');
-        this._drawPoints(this._ctx, this._arrFree);
-        this._drawArray(this._ctx, this._arrMonsters);
-        this._player.draw(this._ctx);
-        this._drawArray(this._ctx, this._arrBalls);
-    },
-
-    _drawArray: function (ctx, array, fillStyle) {
-        for (var i = 0; i < array.length; i++) {
-            var obj = array[i];
-            obj.draw(ctx, fillStyle);
-        }
-    },
-    _drawPoints: function (ctx, array) {
-        for (var i = 0; i < array.length; i++) {
-            var obj = array[i];
-            obj.drawGradientPoints(ctx);
-        }
-    },
-
-    get_enemies: function () {
-        var obstacles = [];
-
-        for (var ball in this._arrBalls) {
-            obstacles.push(this._arrBalls[ball].get_box());
-        }
-        for (var monster in this._arrMonsters) {
-            obstacles.push(this._arrMonsters[monster].get_box());
-        }
-
-        return obstacles;
-    },
-
-    onConquer: function (sourcePolyIndex, trackPoly, innerPath, outerPath) {        
-        var area = 0, poly;
-        var innerContainsBall = false, outerContainsBall = false;;
-        
-        //split
-        var innerPolys = this._arrFree[sourcePolyIndex].split(innerPath);                
-        var outerPolys = this._arrFree[sourcePolyIndex].split(outerPath);
-        
-        //since we now conquered part of the free area, we'll remove it from the list of free areas
-        this._arrFree.splice(sourcePolyIndex, 1);
-        
-        poly = innerPolys[0];
-        for (var b = 0; b < this._arrBalls.length; b++) {            
-            if (poly.containsPoint(this._arrBalls[b].get_center())) {
-                innerContainsBall = true;
-                break;
-            }
-        }
-        
-        poly = outerPolys[1];
-        for (var b = 0; b < this._arrBalls.length; b++) {            
-            if (poly.containsPoint(this._arrBalls[b].get_center())) {
-                outerContainsBall = true;
-                break;
-            }
-        }
-        
-        if (innerContainsBall && outerContainsBall) {
-            //add track polygon to conqured areas
-            this._arrConquered.push(trackPoly);
-            area += Math.abs(trackPoly.get_area());
-            this._arrFree.push(innerPolys[0]);
-            this._arrFree.push(outerPolys[1]);
-        } else if (innerContainsBall && !outerContainsBall) {
-            this._arrConquered.push(innerPolys[1]);
-            area += Math.abs(innerPolys[1].get_area());
-            this._arrFree.push(innerPolys[0]);
-        } else if (!innerContainsBall && outerContainsBall) {
-            this._arrConquered.push(outerPolys[0]);
-            area += Math.abs(outerPolys[0].get_area());
-            this._arrFree.push(outerPolys[1]);
-        } else {
-        }
-        
-        //update score
-        var last = Math.round(this._conquredArea / this._totalArea * 100);
-        this._conquredArea += area;
-        var conqueredPercent = Math.round(this._conquredArea / this._totalArea * 100);
-        this._score += ((conqueredPercent - last) * 10); // Math.round(area / this._width * this._height / 100);
-        
-        //raise event on changes in score and conquered area
-        this._raiseEvent('conquer', conqueredPercent);
-        this._raiseEvent('score', this._score);
-        
-        if (conqueredPercent >= Game.CONQUERED_PERCENT_MINIMUM_LIMIT) //did we reach the minimum limit and going to next level
-        {
-            this.stop();
-            this._numOfBalls++;
-            this._numOfMonsters++;
-            var self = this;
-            setTimeout(function() { self.init(); self.start(); }, 1000);
-        }
-    },
-
-    onFail: function () {
-        this.stop();
-        this._numOfLives--;
-        this._playAudio('fail');
-        this._raiseEvent('fail', this._numOfLives);
-        if (this._numOfLives == 0) {
-            this.end();
-            this._raiseEvent('gameOver', this._score);
-        }
-        else {
-            var self = this;
-            setTimeout(function() { self.relocatePlayer(); self.start(); }, 1000);
-        }
-    },
-
-    _playAudio: function (a) {
-        if (!this._mute && this._audio[a])
-            this._audio[a].play();
+    _random:function (min, max) {
+        var val = min + Math.random() * (max - min);
+        return Math.round(val);
     }
 
-};
-
-Game.prototype = $.extend(
-    {},
-    EventHandler.prototype,
-    Game.prototype
-);
+}
---
+++ scripts/grid.js
@@ -0,0 +1,55 @@
+
+function Grid(rows, cols, state) {
+    this._rows;
+    this._cols;
+    this._grid;
+    this.init(rows, cols, state);
+}
+
+Grid.prototype = {
+
+    init: function(rows, cols, state) {
+        this._rows = rows;
+        this._cols = cols;
+
+        this._grid = new Array(rows);
+        for (var row in this._grid) {
+            this._grid[row] = new Array(cols);
+        }
+
+        for(var i=0; i<rows; i++) {
+            for (var j=0; j<cols; j++) {
+                this.set_state(i, j, state);
+            }
+        }
+    },
+
+    get_state: function(row, col) {
+        return this._grid[row][col];
+    },
+
+    set_state: function(row, col, state) {
+        this_grid[row][col] = state;
+    },
+
+    get_numOfCols: function() {
+        return this._cols;
+    },
+
+    get_numOfRows: function() {
+        return this._rows;
+    },
+
+    set_col: function(col, state) {
+        for (var i=0; i<this._rows; i++) {
+            this.set_state(i,col, state);
+        }
+    },
+
+    set_row: function(row, state) {
+        for (var i=0; i<this._cols; i++) {
+            this.set_state(row, i, state);
+        }
+    }
+
+};
