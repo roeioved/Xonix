5cd225b7558637b80628f8441c69d6988661bd75
Roei <oved.roei@gmail.com>
2012-03-14 16:33:06 +0200

    delete unnecessarily files

--- scripts/gamestate.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by JetBrains WebStorm.
- * User: yairlevinson
- * Date: 3/7/12
- * Time: 2:51 PM
- * To change this template use File | Settings | File Templates.
- */
-
-var GAME_STATES = {RUNNING: 1, STOPPED: 2, ENDED: 3};
