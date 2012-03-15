7a4d6d0015a7a4d3c003612cb4dbd78e5366d6f6
yairlev <yairlevinson@new-host-2.home>
2012-03-15 11:21:57 +0200

    merge changes

--- scripts/game.js
+++ scripts/game.js
@@ -13,8 +13,8 @@ function Game(rows, cols, blockSize, frame, ctx) {
     this._balls = [];
     this._monsters = [];
 
-    this._score = 0;
-    this._level = 1;
+    this._score;
+    this._level;
 
     this._mute = true;
     this._audio = [];
@@ -49,7 +49,38 @@ Game.KEY_CODES = {LEFT:37, UP:38, RIGHT:39, DOWN:40};
 
 Game.prototype = {
 
-    init:function () {
+    init: function () {
+        this._score = 0;
+
+        //reset level
+        this.resetLevel(1);
+
+        this._player.addEventListener('conquer', this.onConquer, this);
+        this._player.addEventListener('fail', this.onFail, this);
+
+        var self = this;
+
+        $(document).keydown(function (event) {
+            switch (event.keyCode) {
+                case Game.KEY_CODES.LEFT:
+                    self._player.moveLeft();
+                    break;
+                case Game.KEY_CODES.UP:
+                    self._player.moveUp();
+                    break;
+                case Game.KEY_CODES.RIGHT:
+                    self._player.moveRight();
+                    break;
+                case Game.KEY_CODES.DOWN:
+                    self._player.moveDown();
+                    break;
+            }
+        });
+    },
+
+    resetLevel: function (level) {
+        this._level = level;
+
         this._grid = new Grid(this._rows, this._cols, Game.FREE_STATE);
 
         this._balls = [];
@@ -65,12 +96,11 @@ Game.prototype = {
 
         //create player
         this._player = new Player(0, Math.floor(this._cols / 2), new Vector(0, 0), this._grid);
-        this._player.addEventListener('conquer', this.onConquer, this);
-        this._player.addEventListener('fail', this.onFail, this);
         this._playerState = Game.CONQUERED_STATE;
 
         //create balls
-        for (var i = 0; i < Game.NUM_OF_BALLS; i++) {
+        var numOfBalls = Game.NUM_OF_BALLS * this._level;
+        for (var i = 0; i < numOfBalls; i++) {
             var col = this._random(this._frame, this._cols - this._frame);
             var row = this._random(this._frame, this._rows - this._frame);
             var velocityX = (this._random(0, 1) == 0 ? -1 : 1);
@@ -82,7 +112,8 @@ Game.prototype = {
         }
 
         //create monsters
-        for (var i = 0; i < Game.NUM_OF_MONSTERS; i++) {
+        var numOfMonsters = Game.NUM_OF_MONSTERS * (this._level - 1);
+        for (var i = 0; i < numOfMonsters; i++) {
             var cols_1 = this._random(0, this._frame - 1);
             var cols_2 = this._random(this._cols - this._frame, this._cols - 1);
             var col = this._random(0, 1) == 0 ? cols_1 : cols_2;
@@ -121,7 +152,7 @@ Game.prototype = {
         });
     },
 
-    refreshScore:function () {
+    refreshScore: function () {
         var total = this._grid.get_size();
         var conqured = this._grid.get_count(Game.CONQUERED_STATE);
         conqured -= (2 * this._rows * this._frame) + (2 * (this._cols - this._frame) * this._frame);
@@ -131,9 +162,13 @@ Game.prototype = {
 
         this._raiseEvent('conquer', pct);
         this._raiseEvent('score', this._score);
+
+        if (pct >= Game.CONQUERED_PERCENT_MINIMUM_LIMIT) {
+            this.nextLevel();
+        }
     },
 
-    start:function () {
+    start: function () {
         var self = this;
 
         this._intervalId = setInterval(function () {
@@ -143,7 +178,7 @@ Game.prototype = {
         }, 1000 / 30);
     },
 
-    step:function () {
+    step: function () {
         for (var i = 0; i < this._monsters.length; i++) {
             this._monsters[i].step();
         }
@@ -194,7 +229,7 @@ Game.prototype = {
         }
     },
 
-    draw:function () {
+    draw: function () {
         this._grid.draw(this._ctx, this._blockSize, Game.FREE_FILL_COLOR, Game.CONQUERED_FILL_COLOR, Game.TRACK_FILL_COLOR);
 
         for (var i = 0; i < this._monsters.length; i++) {
@@ -208,7 +243,7 @@ Game.prototype = {
         this._player.draw(this._ctx, this._blockSize, Game.PLAYER_FILL_COLOR, Game.PLAYER_STROKE_COLOR);
     },
 
-    _random:function (min, max) {
+    _random: function (min, max) {
         var val = min + Math.random() * (max - min);
         return Math.round(val);
     }
