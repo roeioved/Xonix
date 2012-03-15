145d0751106da28a2fcc679adcfb6610e55ac598
yairlev <yairlevinson@new-host-2.home>
2012-03-15 11:07:50 +0200

    implemented the grid's flood method

--- scripts/game.js
+++ scripts/game.js
@@ -4,24 +4,24 @@ function Game(rows, cols, blockSize, frame, ctx) {
     this._blockSize = blockSize;
     this._frame = frame;
     this._ctx = ctx;
-    
+
     this._grid;
-    
+
     this._player;
     this._playerState;
-    
+
     this._balls = [];
     this._monsters = [];
-    
+
     this._score = 0;
     this._level = 1;
-    
+
     this._mute = true;
     this._audio = [];
     this._audio['fail'] = new Audio('sounds/fail.mp3');
 
     this._intervalId;
-    
+
     this.init();
 }
 
@@ -43,17 +43,18 @@ Game.MONSTER_STROKE_COLOR = '#000000';
 Game.FREE_STATE = 0;
 Game.CONQUERED_STATE = 1;
 Game.TRACK_STATE = 2;
+Game.FLOOD_STATE = 3;
 
 Game.KEY_CODES = {LEFT:37, UP:38, RIGHT:39, DOWN:40};
 
 Game.prototype = {
-    
-    init: function () {
-        this._grid = new Grid(this._rows, this._cols, Game.FREE_STATE);        
-        
+
+    init:function () {
+        this._grid = new Grid(this._rows, this._cols, Game.FREE_STATE);
+
         this._balls = [];
         this._monsters = [];
-        
+
         //build frames
         for (var i = 0, j = this._cols - 1, k = this._rows - 1; i < this._frame; i++, j--, k--) {
             this._grid.set_row(i, Game.CONQUERED_STATE);
@@ -61,47 +62,47 @@ Game.prototype = {
             this._grid.set_col(i, Game.CONQUERED_STATE);
             this._grid.set_col(j, Game.CONQUERED_STATE);
         }
-        
+
         //create player
         this._player = new Player(0, Math.floor(this._cols / 2), new Vector(0, 0), this._grid);
         this._player.addEventListener('conquer', this.onConquer, this);
         this._player.addEventListener('fail', this.onFail, this);
         this._playerState = Game.CONQUERED_STATE;
-        
+
         //create balls
         for (var i = 0; i < Game.NUM_OF_BALLS; i++) {
             var col = this._random(this._frame, this._cols - this._frame);
             var row = this._random(this._frame, this._rows - this._frame);
             var velocityX = (this._random(0, 1) == 0 ? -1 : 1);
             var velocityY = (this._random(0, 1) == 0 ? -1 : 1);
-            
+
             var ball = new Ball(row, col, new Vector(velocityX, velocityY), this._grid, Game.CONQUERED_STATE);
-            
+
             this._balls.push(ball);
         }
-        
+
         //create monsters
         for (var i = 0; i < Game.NUM_OF_MONSTERS; i++) {
             var cols_1 = this._random(0, this._frame - 1);
             var cols_2 = this._random(this._cols - this._frame, this._cols - 1);
             var col = this._random(0, 1) == 0 ? cols_1 : cols_2;
-            
+
             var rows_1 = this._random(0, this._frame - 1);
             var rows_2 = this._random(this._rows - this._frame, this._rows - 1);
             var row = this._random(0, 1) == 0 ? rows_1 : rows_2;
-            
+
             var velocityX = this._random(0, 1) == 0 ? -1 : 1;
             var velocityY = this._random(0, 1) == 0 ? -1 : 1;
-            
+
             var monster = new Monster(row, col, new Vector(velocityX, velocityY), this._grid, Game.FREE_STATE);
             this._monsters.push(monster);
         }
-        
+
         //refresh score
         this.refreshScore();
-        
+
         var self = this;
-        
+
         $(document).keydown(function (event) {
             switch (event.keyCode) {
                 case Game.KEY_CODES.LEFT:
@@ -119,22 +120,22 @@ Game.prototype = {
             }
         });
     },
-    
-    refreshScore: function () {
+
+    refreshScore:function () {
         var total = this._grid.get_size();
         var conqured = this._grid.get_count(Game.CONQUERED_STATE);
         conqured -= (2 * this._rows * this._frame) + (2 * (this._cols - this._frame) * this._frame);
-        
+
         var pct = Math.round(conqured / total * 100);
         this._score += pct * 10;
-        
+
         this._raiseEvent('conquer', pct);
         this._raiseEvent('score', this._score);
     },
-    
-    start: function () {
+
+    start:function () {
         var self = this;
-        
+
         this._intervalId = setInterval(function () {
             self._ctx.clearRect(0, 0, self._cols * self._blockSize, self._rows * self._blockSize);
             self.step();
@@ -142,17 +143,17 @@ Game.prototype = {
         }, 1000 / 30);
     },
 
-    step: function () {
+    step:function () {
         for (var i = 0; i < this._monsters.length; i++) {
             this._monsters[i].step();
         }
-        
+
         for (var i = 0; i < this._balls.length; i++) {
             this._balls[i].step();
         }
-        
+
         this._player.step();
-        
+
         var row = this._player.get_row();
         var col = this._player.get_col();
         var state = this._grid.get_state(row, col);
@@ -161,34 +162,57 @@ Game.prototype = {
             this._grid.set_state(row, col, Game.TRACK_STATE);
         } else if (state == Game.CONQUERED_STATE && this._playerState == Game.TRACK_STATE) {
             this._playerState = Game.CONQUERED_STATE;
-            this._player.stop();            
+            this._player.stop();
             this._grid.replace(Game.TRACK_STATE, Game.CONQUERED_STATE);
-            
-            //todo:fill algorithm
-            
+            this._grid.flood(Game.FREE_STATE, Game.FLOOD_STATE);
+
+            var floodContainsBall = false, freeContainsBall = false;
+
+            for (var ball in this._balls) {
+                ball = this._balls[ball];
+                var state = this._grid.get_state(ball.get_row(), ball.get_col());
+
+                if (state == Game.FLOOD_STATE) {
+                    floodContainsBall = true;
+                }
+                else if (state == Game.FREE_STATE) {
+                    freeContainsBall = true;
+                }
+            }
+
+            if (!freeContainsBall) {
+                this._grid.replace(Game.FREE_STATE, Game.CONQUERED_STATE);
+            }
+            if (floodContainsBall) {
+                this._grid.replace(Game.FLOOD_STATE, Game.FREE_STATE);
+            } else {
+                this._grid.replace(Game.FLOOD_STATE, Game.CONQUERED_STATE);
+            }
+
+
             this.refreshScore();
         }
     },
 
-    draw: function () {
+    draw:function () {
         this._grid.draw(this._ctx, this._blockSize, Game.FREE_FILL_COLOR, Game.CONQUERED_FILL_COLOR, Game.TRACK_FILL_COLOR);
-        
+
         for (var i = 0; i < this._monsters.length; i++) {
             this._monsters[i].draw(this._ctx, this._blockSize, Game.MONSTER_FILL_COLOR, Game.MONSTER_STROKE_COLOR);
         }
-        
+
         for (var i = 0; i < this._balls.length; i++) {
             this._balls[i].draw(this._ctx, this._blockSize, Game.BALL_FILL_COLOR, Game.BALL_STROKE_COLOR);
         }
-        
+
         this._player.draw(this._ctx, this._blockSize, Game.PLAYER_FILL_COLOR, Game.PLAYER_STROKE_COLOR);
     },
-    
-    _random: function (min, max) {
+
+    _random:function (min, max) {
         var val = min + Math.random() * (max - min);
         return Math.round(val);
     }
-    
+
 }
 
 Game.prototype = $.extend(
--- scripts/grid.js
+++ scripts/grid.js
@@ -6,70 +6,93 @@ function Grid(rows, cols, state) {
 }
 
 Grid.prototype = {
-    
-    init: function(rows, cols, state) {
+
+    init:function (rows, cols, state) {
         this._rows = rows;
         this._cols = cols;
-        
+
         this._grid = new Array(rows);
-        for (var i=0; i< this._grid.length; i++) {
+        for (var i = 0; i < this._grid.length; i++) {
             this._grid[i] = new Array(cols);
         }
-        
-        for(var i=0; i<rows; i++) {
-            for (var j=0; j<cols; j++) {
+
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
                 this.set_state(i, j, state);
             }
         }
     },
 
-    get_state: function(row, col) {
+    get_state:function (row, col) {
         return this._grid[row][col];
     },
-    
-    set_state: function(row, col, state) {
+
+    set_state:function (row, col, state) {
         this._grid[row][col] = state;
     },
-    
-    get_numOfCols: function() {
+
+    get_numOfCols:function () {
         return this._cols;
     },
-    
-    get_numOfRows: function() {
+
+    get_numOfRows:function () {
         return this._rows;
     },
-    
-    set_col: function(col, state) {
-        for (var i=0; i<this._rows; i++) {
-            this.set_state(i,col, state);
+
+    set_col:function (col, state) {
+        for (var i = 0; i < this._rows; i++) {
+            this.set_state(i, col, state);
         }
     },
-    
-    set_row: function(row, state) {
-        for (var i=0; i<this._cols; i++) {
+
+    set_row:function (row, state) {
+        for (var i = 0; i < this._cols; i++) {
             this.set_state(row, i, state);
         }
     },
-    
-    get_size: function () {
+
+    get_size:function () {
         return this._rows * this._cols;
     },
-    
-    get_count: function (state) {
+
+    get_count:function (state) {
         var counter = 0;
-        
-        for(var i=0; i<this._rows; i++) {
-            for (var j=0; j<this._cols; j++) {
+
+        for (var i = 0; i < this._rows; i++) {
+            for (var j = 0; j < this._cols; j++) {
                 if (this.get_state(i, j) == state) {
                     counter++;
                 }
             }
         }
-        
+
         return counter;
     },
 
-    findFirst: function (state) {
+    flood:function (oldValue, newValue) {
+        var cell = this._findFirst(oldValue);
+        if (cell) {
+            this._recursiveFlood(cell.row, cell.col, oldValue, newValue);
+        }
+    },
+
+    _recursiveFlood:function (row, col, oldValue, newValue) {
+        for (var i = -1; i <= 1; i++) {
+            var r = row + i;
+            for (var j = -1; j <= 1; j++) {
+                var c = col + j;
+                if (r >= 0 && r < this._rows && c >= 0 && c < this._cols) {
+                    var state = this.get_state(r, c);
+                    if (state == oldValue) {
+                        this.set_state(r, c, newValue);
+                        this._recursiveFlood(r, c, oldValue, newValue);
+                    }
+                }
+            }
+        }
+    },
+
+    _findFirst: function (state) {
         for(var j=0; j<this._cols; j++) {
             for (var i=0; i<this._rows; i++) {
                 if (this.get_state(i, j) == state) {
@@ -80,30 +103,36 @@ Grid.prototype = {
         return null;
     },
 
-    replace: function(oldValue, newValue) {
-        for(var i=0; i<this._rows; i++) {
-            for (var j=0; j<this._cols; j++) {
+    replace:function (oldValue, newValue) {
+        for (var i = 0; i < this._rows; i++) {
+            for (var j = 0; j < this._cols; j++) {
                 var state = this.get_state(i, j);
-                
+
                 if (state == oldValue) {
                     this.set_state(i, j, newValue);
                 }
             }
         }
     },
-        
-    draw: function(ctx, blockSize, color0, color1, color2) {
-        for(var i=0; i<this._rows; i++) {
-            for (var j=0; j<this._cols; j++) {
+
+    draw:function (ctx, blockSize, color0, color1, color2) {
+        for (var i = 0; i < this._rows; i++) {
+            for (var j = 0; j < this._cols; j++) {
                 var state = this.get_state(i, j);
                 var fillStyle;
-                
+
                 switch (state) {
-                    case 0: fillStyle = color0; break;
-                    case 1: fillStyle = color1; break;
-                    case 2: fillStyle = color2; break;
+                    case 0:
+                        fillStyle = color0;
+                        break;
+                    case 1:
+                        fillStyle = color1;
+                        break;
+                    case 2:
+                        fillStyle = color2;
+                        break;
                 }
-                
+
                 ctx.fillStyle = fillStyle;
                 ctx.beginPath();
                 ctx.rect(j * blockSize, i * blockSize, blockSize, blockSize);
@@ -112,5 +141,14 @@ Grid.prototype = {
             }
         }
     }
-    
+
 };
+
+
+Grid.State = {
+    Free:0,
+    Occupied:1,
+    Path:2,
+    Flood1:3,
+    Flood2:4
+}
