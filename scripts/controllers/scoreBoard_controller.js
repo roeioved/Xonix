2f146582382dc423e86f55a193cb7b094f285b5a
yairlev <yairlevinson@new-host-2.home>
2012-03-22 18:48:35 +0200

    leaderboard not saved on server - fixed

--- scripts/controllers/scoreBoard_controller.js
+++ scripts/controllers/scoreBoard_controller.js
@@ -1,29 +1,33 @@
 $.Controller('ScoreBoard', {
-    init: function() {
+    init:function () {
     },
-    
-    show: function(scoreBoard) {
-        if (! this._root) {
-            this._root = $($.View('./views/score_board.ejs', scoreBoard));
-            this._root.appendTo($('#container'));
-        }
-        this._root.show();
-        
-        var self = this;
-        $(document).bind('keydown.sb', function(e) { self._onEnter.call(self, e); });
-    },
-    
-    hide: function() {
+
+    show:function (scoreBoard) {
+
         if (this._root)
-        {
+            this._root.remove();
+
+        this._root = $($.View('./views/score_board.ejs', scoreBoard));
+        this._root.appendTo($('#container'));
+
+
+        this._root.show();
+
+        var self = this;
+        $(document).bind('keydown.sb', function (e) {
+            self._onEnter.call(self, e);
+        });
+    },
+
+    hide:function () {
+        if (this._root) {
             this._root.hide();
         }
         $(document).unbind('keydown.sb');
     },
-    
-    _onEnter: function(e) {
-        if (e.which == 13)
-        {
+
+    _onEnter:function (e) {
+        if (e.which == 13) {
             this._raiseEvent('enter');
         }
     }
